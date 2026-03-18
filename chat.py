afe1f442d6f7fd0fc67622ca33398a91f50de093
tmanbarton <54116906+tmanbarton@users.noreply.github.com>
2026-03-18 15:07:09 -0600

    Integrate RAG into the chatbot

--- chat.py
+++ chat.py
@@ -4,8 +4,6 @@ import anthropic
 import numpy as np
 from sentence_transformers import SentenceTransformer
 
-client = anthropic.Anthropic(api_key=os.environ.get("ANTHROPIC_API_KEY_CLI_CHATBOT"))
-
 system_message = """
         Your only task is to answer questions about the if-engine API, a reusable Java-based engine and builder for creating interactive fiction games, via its README.
         You speak as if you're from the 1870s - the Gold Rush era. Some attributes of how 1870's folks and how you speak:
@@ -44,8 +42,11 @@ system_message = """
             - "tolerable" — "tolerable fair," "tolerable worn out"
             - "I reckon," "I expect," "I allow"
         Again, THESE ARE NOT EXACT PATTERNS TO FOLLOW!! THEY ARE SUGGESTIONS SO YOU KNOW WHAT 1870s FOLKS SOUND LIKE!!
+        Despite being from the 1870's, use code blocks to answer users' questions when it makes sense.
+        Also note, this is CLI based, so you don't have access to em dashes. Use "word - word" for em dashes instead of "word-word".
         """
 
+# Get if-engine README embeddings from local json file as an array
 with open("index.json") as f:
     index = json.load(f)
 
@@ -53,25 +54,34 @@ chunks = [item["text"] for item in index]
 embeddings = np.array([item["embedding"] for item in index])
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
+# The chatbot. Loop until "quit" is entered or program is stopped
 messages = []
 while True:
     user_input = input("~ ")
     if user_input == "quit":
         break
 
-    input_embedding = model.encode(user_input)[0]
+    # Embed user input and compare to stored embedding to get closest 3 matches
+    input_embedding = model.encode([user_input])[0]
     scores = np.dot(embeddings, input_embedding) / (np.linalg.norm(embeddings, axis=1) * np.linalg.norm(input_embedding))
+    top_3 = np.argsort(scores)[::-1][:3]
 
-    top_3 = np.sort(scores)[::-1][:3]
+    # Prepare user message to send to LLM. Contains user input and the documentation found
+    context = "\n\n".join(chunks[i] for i in top_3)
+    augmented_input = f"Question:\n{user_input}\n\nDocumentation context:\n{context}"
+    messages.append({"role": "user", "content": augmented_input})
 
-    messages.append({"role": "user", "content": user_input})
-
+    # Send message to LLM
+    client = anthropic.Anthropic(api_key=os.environ.get("ANTHROPIC_API_KEY_CLI_CHATBOT"))
     response = client.messages.create(
         model = "claude-haiku-4-5",
         max_tokens = 1024,
         system = system_message,
         messages = messages)
 
+    # Replace user message (last element in message array) with only user input. Don't want documentation in conversation history.
+    # Append assistant message to conversation history and print
+    messages[-1]["content"] = user_input
     response_text = response.content[0].text
     messages.append({"role": "assistant", "content": response_text})
     print(f"~ {response_text}")
